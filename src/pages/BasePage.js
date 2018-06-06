65896ee5cdeadc6a60d8f66983393610629d7f4d
Bynn <865068275@qq.com>
2018-06-06 19:49:09 +0800

    update
    custom page lifecycle

--- src/pages/BasePage.js
+++ src/pages/BasePage.js
@@ -12,40 +12,69 @@ export default class BasePage extends BaseComponent {
 
   BasicStyle = BasicStyle;
 
+  /* ---自定义Page生命周期---
+   *  
+   *  子类可重写
+   * 
+   */
+  onStart() { } //Page开始创建
+  onCreate() { } //Page创建完成
+  onWillFocus() { } //Page即将出现
+  onDidFocus() { } //Page出现 
+  onWillBlur() { } //Page即将消失
+  onDidBlur() { } //Page消失
+  onDestroy() { } //Page销毁
+
   constructor(props) {
     super(props);
     this.initSubscriptions();
   }
 
+  componentWillMount() {
+    // console.log('componentWillMount');
+    this.onStart();
+  }
+
+  componentDidMount() {
+    // console.log('componentDidMount');
+    this.onCreate();
+  }
+
   componentWillUnmount() {
-    this.willFocusSubscription && this.willFocusSubscription.remove();
-    this.didFocusSubscription && this.didFocusSubscription.remove();
-    this.willBlurSubscription && this.willBlurSubscription.remove();
-    this.didBlurSubscription && this.didBlurSubscription.remove();
+    // console.log('componentWillUnmount');
+    this.subscriptions && this.subscriptions.forEach((sub) => sub.remove());
+    this.onDestroy();
   }
 
   initSubscriptions() {
     const { navigation } = this.props;
-    this.willFocusSubscription = navigation.addListener( 'willFocus', payload => this.willFocus(payload));
-    this.didFocusSubscription = navigation.addListener( 'didFocus', payload => this.didFocus(payload));
-    this.willBlurSubscription = navigation.addListener( 'willBlur', payload => this.willBlur(payload));
-    this.didBlurSubscription = navigation.addListener( 'didBlur', payload => this.didBlur(payload));
+    this.subscriptions = [
+      navigation.addListener( 'willFocus', payload => this.willFocus(payload)),
+      navigation.addListener( 'didFocus', payload => this.didFocus(payload)),
+      navigation.addListener( 'willBlur', payload => this.willBlur(payload)),
+      navigation.addListener( 'didBlur', payload => this.didBlur(payload))
+    ]
   }
 
   willFocus(payload) {
-
+    // console.log('willFocus');
+    this.onWillFocus();
   }
 
-  didFocus(payload) {
-
+  didFocus(payload) { 
+    // console.log('didFocus');
+    this.onDidFocus();
   }
 
   willBlur(payload) {
+    // console.log('willBlur');
     Keyboard.dismiss();
+    this.onWillBlur();
   }
 
   didBlur(payload) {
-    
+    // console.log('didBlur');
+    this.onDidBlur();
   }
 
   getProps() {
