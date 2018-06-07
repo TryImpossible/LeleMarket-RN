0a298d502aab1d08f930e2b6cb972cdbcebb6440
Bynn <865068275@qq.com>
2018-06-07 14:25:04 +0800

    update
    1.modify file directory
    2.full search function

---
+++ src/pages/other/SearchPage.js
@@ -0,0 +1,209 @@
+import React from 'react';
+
+import {
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  FlatList,
+  TouchableOpacity,
+  requireNativeComponent,
+  propTypes,
+  processColor, //字符Color转换为数字 
+  Animated,
+  Easing
+} from 'react-native';
+
+import BasePage from '../BasePage';
+
+import NavBar from '../../widgets/NavBar';
+
+import SvgUri from '../../dependencies/react-native-svg-uri';
+
+import SeparatorLine from '../../widgets/SeparatorLine';
+
+import PropTypes from 'prop-types';
+
+import hotSearch from './hotSearch.json';
+
+export default class SearchPage extends BasePage {
+
+  constructor(props) {
+    super(props);
+    this.path = new Animated.Value(0); //输入框动画
+    this.state = {
+      key: '',
+      data: [
+        { key: `section1`, text: '巧克力' },
+        { key: `section1`, text: '衣服' },
+        { key: `section1`, text: '服装' },
+        { key: `section1`, text: 'cosplay' },
+        { key: `section1`, text: '天涯明月刀' },
+        { key: `section1`, text: '天涯明月刀 cosplay' }
+      ]
+    }
+  }
+
+  /**
+   * 测试外部链接跳转至App
+   */
+  // render() {
+  //   return (
+  //     <View style={this.BasicStyle.pageContainer}>
+  //       <NavBar leftText={'返回'} title={'外部链接跳转至App'} />
+  //       <Text>需要搜索的参数: {this.getProps().param}</Text>
+  //     </View>
+  //   )
+  // }
+
+  onCreate() {
+
+  }
+
+  render() {
+    const NavBarTitleView = (
+      <View style={styles.NavBarTitleView}>
+        <SvgUri width={getSize(20)} height={getSize(20)} source={'icon_search'} />
+        <TextInput
+          ref={ref => this.textInput = ref}
+          style={{ marginHorizontal: getSize(5), flex: 1, fontSize: getSize(14), color: '#333333' }}
+          autoFocus={true}
+          placeholder={'请输入商品名称'}
+          placeholderTextColor='#bebec4'
+          value={this.state.key}
+          onChangeText={this.inputChange} />
+        <Animated.View
+          onStartShouldSetResponder={() => true}
+          onResponderGrant={this.close}
+          style={[styles.NavBarColseView, { transform: [{ rotateZ: '45deg' }], opacity: this.path }]} >
+          <View style={{ position: 'absolute', width: getSize(2), height: getSize(10), borderRadius: getSize(1), backgroundColor: '#fff' }} />
+          <View style={{ position: 'absolute', width: getSize(10), height: getSize(2), borderRadius: getSize(1), backgroundColor: '#fff' }} />
+        </Animated.View>
+      </View>
+    );
+    return (
+      <View style={styles.container}>
+        <NavBar showLeftView={false} titleView={NavBarTitleView} rightText={'取消'} rightPress={() => this.goBack()} />;
+        <FlatList
+          data={this.state.data}
+          ListHeaderComponent={() => {
+            return <HotSearchCell data={this.state.data} item={hotSearch.data} select={(i) => this.showToast('')} del={() => this.showToast('')} />;
+          }}
+          renderItem={({ item, index }) => {
+            console.log(item, index);
+            return <LatelySearchCell item={item} index={index} onPress={() => this.props.showToast(item.text)} />
+          }}
+          ItemSeparatorComponent={() => <SeparatorLine left={getSize(10)} width={Const.SCREEN_WIDTH - getSize(10)} />}
+          keyExtractor={(item, index) => `sectionList${index}`}
+          getItemLayout={(data, index) => ({ length: getSize(44), offset: getSize(44) * index, index })} />
+      </View>
+    )
+  }
+
+  /**
+   * 输入
+   */
+  inputChange = (text) => {
+    this.setState({ key: text });
+    if (text && text.trim().length > 0 && Math.round(this.path._value) === 0) {
+      Animated.timing(this.path, {
+        toValue: 1,
+        easing: Easing.ease,
+        duration: 50
+      }).start();
+    } else if (!text && Math.round(this.path._value) === 1) {
+      Animated.timing(this.path, {
+        toValue: 0,
+        easing: Easing.ease,
+        duration: 50
+      }).start();
+    }
+  }
+
+  /**
+   * 关闭
+   */
+  close = () => {
+    this.setState({ key: '' });
+    this.textInput && this.textInput.clear();
+    Animated.timing(this.path, {
+      toValue: 0,
+      easing: Easing.ease,
+      duration: 50
+    }).start();
+  }
+}
+
+/**
+ * 热门搜索
+ * @param {*} props 
+ */
+const HotSearchCell = props => {
+  const { data, item, select, del } = props;
+  return (
+    <View>
+      <View style={{ paddingHorizontal: getSize(10), paddingVertical: getSize(8) }}>
+        <Text style={{ marginBottom: getSize(8), color: '#6c6c6c', fontSize: getSize(13) }}>热门搜索</Text>
+        <View style={{ flexDirection: 'row', flexWrap: 'wrap' }}>
+          {
+            item.map((item, index) => {
+              return (
+                <TouchableOpacity key={`HotSearch${index}`} activeOpacity={Const.ACTIVE_OPACITY} onPress={select} style={{ justifyContent: 'center', alignItems: 'center', borderColor: Const.LINE_COLOR, borderWidth: Const.LINE_WIDTH, borderRadius: getSize(12), paddingHorizontal: getSize(6), paddingVertical: getSize(4), marginRight: getSize(8), marginBottom: getSize(8) }} >
+                  <Text style={{ fontSize: getSize(11), letterSpacing: getSize(2), color: '#333333' }}>{item.name}</Text>
+                </TouchableOpacity>
+              )
+            })
+          }
+        </View>
+      </View>
+      {data.length === 0 ? null : <View style={{ width: Const.SCREEN_WIDTH, height: getSize(5), backgroundColor: '#f1f1f1' }} />}
+      {data.length === 0 ? null : <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+        <Text style={{ paddingLeft: getSize(10), color: '#6c6c6c', fontSize: getSize(13) }}>最近搜索</Text>
+        <TouchableOpacity activeOpacity={Const.ACTIVE_OPACITY} onPress={del} style={{ paddingVertical: getSize(8), paddingHorizontal: getSize(10) }}>
+          <SvgUri width={getSize(20)} height={getSize(20)} source={'icon_del'} />
+        </TouchableOpacity>
+      </View>}
+    </View>
+  )
+}
+
+/**
+ * 最近搜索
+ * @param {*} props 
+ */
+const LatelySearchCell = props => {
+  const { item, index, onPress } = props;
+  return (
+    <TouchableOpacity activeOpacity={Const.ACTIVE_OPACITY} onPress={onPress} style={{ width: Const.SCREEN_WIDTH, height: getSize(44), flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', paddingHorizontal: getSize(10), paddingVertical: getSize(8) }}>
+      <Text style={{ fontSize: getSize(15), color: '#333333' }}>{item.text}</Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    backgroundColor: '#fff'
+  },
+  NavBarTitleView: {
+    marginLeft: getSize(10),
+    width: getSize(310),
+    height: getSize(30),
+    paddingHorizontal: getSize(8),
+    borderRadius: getSize(15),
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    backgroundColor: '#f3f4f4'
+  },
+  NavBarColseView: {
+    width: getSize(18),
+    height: getSize(18),
+    borderRadius: getSize(9),
+    backgroundColor: '#c2c3c3',
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+});
