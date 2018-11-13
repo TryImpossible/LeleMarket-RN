d7a5b29ed50efdeca6a600caee5ac65c279faa78
Barry Xie <barry.xie@antcosa.com>
2018-11-13 11:40:30 +0800

    更新各种配置信息

--- DIY2.0/.eslintrc.js
+++ DIY2.0/.eslintrc.js
@@ -17,8 +17,15 @@ module.exports = {
     fetch: true
   },
   rules: {
-    'no-undef': 'off',
-    'no-useless-constructor': 'off',
+    // Allow .js files to use JSX syntax
+    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-filename-extension.md
+    'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx'] }],
+    // Recommend not to leave any console.log in your code
+    // Use console.error, console.warn and console.info instead
+    // https://eslint.org/docs/rules/no-console
+    'no-console': ['error'],
+    // Forbid the use of extraneous packages
+    // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-extraneous-dependencies.md
     'import/no-extraneous-dependencies': [
       'error',
       {
@@ -27,10 +34,23 @@ module.exports = {
         peerDependencies: false
       }
     ],
-    'react/jsx-filename-extension': ['warn', { extensions: ['.js', '.jsx'] }],
-    'no-return-assign': 'off',
-    'import/prefer-default-export': 'off',
-    'no-param-reassign': 'off',
-    'import/no-unresolved': 0
+    // ESLint plugin for prettier formatting
+    // https://github.com/prettier/eslint-plugin-prettier
+    'prettier/prettier': 'error',
+    // https://eslint.org/docs/rules/no-param-reassign
+    'no-param-reassign': 'warn',
+    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/require-default-props.md
+    'react/require-default-props': ['off', { forbidDefaultForRequired: false }],
+    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/forbid-prop-types.md
+    'react/forbid-prop-types': [
+      'error',
+      {
+        forbid: [],
+        checkContextTypes: false,
+        checkChildContextTypes: false
+      }
+    ],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always']
   }
 };
