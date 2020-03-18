ceb1baadde396584c2ccb167f141bbf3a9d8274b
Barry <865068275@qq.com>
2020-03-18 20:57:30 +0800

    配置Typescript、eslint、prettier

--- LeleMarket2.0/.eslintrc.js
+++ LeleMarket2.0/.eslintrc.js
@@ -1,4 +1,12 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: ['@react-native-community', 'prettier', 'plugin:prettier/recommended'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react', 'prettier'],
+  rules: {
+    // 禁止使用 var
+    'no-var': 'error',
+    // 优先使用 interface 而不是 type
+    '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
+  },
 };
--- LeleMarket2.0/App.tsx
+++ LeleMarket2.0/App.tsx
@@ -6,15 +6,8 @@
  * @flow
  */
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import React, { Props } from 'react';
+import { SafeAreaView, StyleSheet, ScrollView, View, Text, StatusBar } from 'react-native';
 
 import {
   Header,
@@ -24,14 +17,12 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-const App: () => React$Node = () => {
+const App: React.FC = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
+        <ScrollView contentInsetAdjustmentBehavior="automatic" style={styles.scrollView}>
           <Header />
           {global.HermesInternal == null ? null : (
             <View style={styles.engine}>
@@ -42,8 +33,9 @@ const App: () => React$Node = () => {
             <View style={styles.sectionContainer}>
               <Text style={styles.sectionTitle}>Step One</Text>
               <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
+                Edit
+                <Text style={styles.highlight}>App.js</Text>
+                to change this screen and then come back to see your edits.
               </Text>
             </View>
             <View style={styles.sectionContainer}>
