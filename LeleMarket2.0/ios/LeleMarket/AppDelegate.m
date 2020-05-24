d7b2a3400cef9143952ce88858d49b87e1cbe0fe
Barry <865068275@qq.com>
2020-05-24 22:51:45 +0800

    ios端集成react-native-config

--- LeleMarket2.0/ios/LeleMarket/AppDelegate.m
+++ LeleMarket2.0/ios/LeleMarket/AppDelegate.m
@@ -3,6 +3,7 @@
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import "ReactNativeConfig.h"
 
 #if DEBUG
 #import <FlipperKit/FlipperClient.h>
@@ -43,6 +44,16 @@ static void InitializeFlipper(UIApplication *application) {
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  
+  // then read individual keys like:
+  NSString *ENV = [ReactNativeConfig envFor:@"ENV"];
+  NSLog(@"ENV=%@", ENV);
+
+  // or just fetch the whole config
+  NSDictionary *config = [ReactNativeConfig env];
+  
+  NSDictionary *infoDictionary = [[NSBundle mainBundle] infoDictionary];
+  
   return YES;
 }
 
